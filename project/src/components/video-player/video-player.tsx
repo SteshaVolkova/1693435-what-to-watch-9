1e1f5e7af665dd063074506b9c16e5111c6a094c
SteshaVolkova <stepashechka5@gmail.com>
2022-02-26 18:45:04 +0300

    Добваила функционал для сброса фильма при удалении курсора с карточки фильма

--- project/src/components/video-player/video-player.tsx
+++ project/src/components/video-player/video-player.tsx
@@ -10,23 +10,9 @@ type VideoPlayerProps = {
 function VideoPlayer({autoPlay, film}: VideoPlayerProps): JSX.Element {
   const {previewVideoLink, posterImage, id, name} = film;
 
-  const [isLoading, setIsLoading] = useState(true);
   const [isPlaying, setIsPlaying] = useState(autoPlay);
 
   const videoRef = useRef<HTMLVideoElement | null>(null);
-
-  useEffect(() => {
-    if (videoRef.current !== null) {
-      videoRef.current.onloadeddata = () => setIsLoading(false);
-    }
-
-    return () => {
-      if (videoRef.current !== null) {
-        videoRef.current.onloadeddata = null;
-        videoRef.current = null;
-      }
-    };
-  }, [previewVideoLink]);
 
   useEffect(() => {
     if (videoRef.current === null) {
@@ -40,17 +26,18 @@ function VideoPlayer({autoPlay, film}: VideoPlayerProps): JSX.Element {
 
     videoRef.current.pause();
     videoRef.current.currentTime = 0;
-  }, [isPlaying]);
+    videoRef.current.load();
+  }, [isPlaying, posterImage]);
 
   return (
-    <button className="small-film-card catalog__films-card" type="button" disabled={isLoading} onMouseEnter={() => setTimeout(() => setIsPlaying(!isPlaying), 1000)} onMouseLeave={() => setIsPlaying(!isPlaying)}>
+    <article className="small-film-card catalog__films-card" onMouseEnter={() => setIsPlaying(!isPlaying)} onMouseLeave={() => setIsPlaying(!isPlaying)}>
       <div className="small-film-card__image">
         <video ref={videoRef} src={previewVideoLink} poster={posterImage} width="100%" height="100%"></video>
       </div>
       <h3 className="small-film-card__title">
         <Link className="small-film-card__link" to={`/films/${id}`}>{name}</Link>
       </h3>
-    </button>
+    </article>
   );
 }
 
