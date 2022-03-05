817fac3d8d31d5137b85c48a65f5b1fb50ca75db
SteshaVolkova <stepashechka5@gmail.com>
2022-03-05 11:15:07 +0300

    Подправила страницу 404

--- project/src/pages/not-found-page/not-found-page.tsx
+++ project/src/pages/not-found-page/not-found-page.tsx
@@ -1,13 +1,21 @@
 import {Link} from 'react-router-dom';
+import HeaderLogo from '../../components/header-logo/header-logo';
+import Footer from '../../components/footer/footer';
 
 function NotFoundPage(): JSX.Element {
   return (
-    <div className="page-content">
-      <section className="catalog">
+    <div className="user-page">
+      <header className="page-header user-page__head">
+        <HeaderLogo />
+      </header>
+
+      <div className="sign-in user-page__content">
         <h1>404. Page not found</h1>
         <br></br>
         <Link to="/">Go to main page</Link>
-      </section>
+      </div>
+
+      <Footer />
     </div>
   );
 }
