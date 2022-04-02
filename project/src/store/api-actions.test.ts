b4e2d594210528e2ca111a63989fbb3d1d7732b4
SteshaVolkova <stepashechka5@gmail.com>
2022-04-02 21:23:26 +0300

    Добавила тесты ещё для нескольких асинхронных действий

--- project/src/store/api-actions.test.ts
+++ project/src/store/api-actions.test.ts
@@ -3,10 +3,17 @@ import thunk, { ThunkDispatch } from 'redux-thunk';
 import MockAdapter from 'axios-mock-adapter';
 import { configureMockStore } from '@jedmao/redux-mock-store';
 import { createAPI } from '../services/api';
-import { checkAuthAction } from './api-actions';
+import { checkAuthAction, fetchFavoriteFilm, fetchFilmsAction, fetchPromoAction, fetchSimilarFilmsAction, loginAction, logoutAction } from './api-actions';
 import { requireAuthorization } from './user-process/user-process';
 import { APIRoute } from '../const';
 import { State } from '../types/state';
+import { makeFakeFilmsItem } from '../utils/mocks';
+import { loadFilms } from './films-data/films-data';
+import { AuthData } from '../types/auth-data';
+import { setPromoFilm } from './promo-film-data/promo-film-data';
+import { setSimilarFilms } from './similar-films-data/similar-films-data';
+import { datatype } from 'faker';
+import { loadFavoriteFilms } from './favorite-films-data/favorite-films-data';
 
 describe('Async actions', () => {
   const api = createAPI();
@@ -32,5 +39,74 @@ describe('Async actions', () => {
     const actions = store.getActions().map(({type}) => type);
 
     expect(actions).toContain(requireAuthorization.toString());
+  });
+
+  it('should dispatch RequriedAuthorization and RedirectToRoute when POST /login', async () => {
+    const fakeUser: AuthData = {login: 'test@test.ru', password: '123456'};
+
+    mockAPI
+      .onPost(APIRoute.Login)
+      .reply(200, {token: 'secret'});
+
+
+    const store = mockStore();
+    Storage.prototype.setItem = jest.fn();
+
+    await store.dispatch(loginAction(fakeUser));
+
+    const actions = store.getActions().map(({type}) => type);
+
+    expect(actions).toContain(requireAuthorization.toString());
+
+    expect(Storage.prototype.setItem).toBeCalledTimes(1);
+    expect(Storage.prototype.setItem).toBeCalledWith('wtw-token', 'secret');
+  });
+
+  it('should dispatch Load_Films when GET /films', async () => {
+    const mockFilms = new Array(10).fill(null).map(() => ({makeFakeFilmsItem}));
+    mockAPI
+      .onGet(APIRoute.Films)
+      .reply(200, mockFilms);
+
+    const store = mockStore();
+
+    await store.dispatch(fetchFilmsAction());
+
+    const actions = store.getActions().map(({type}) => type);
+
+    expect(actions).toContain(loadFilms.toString());
+  });
+
+  it('should dispatch Load_Promo_Films when GET /promoFilm', async () => {
+    const mockPromoFilm = makeFakeFilmsItem();
+    mockAPI
+      .onGet(APIRoute.PromoFilm)
+      .reply(200, mockPromoFilm);
+
+    const store = mockStore();
+
+    await store.dispatch(fetchPromoAction());
+
+    const actions = store.getActions().map(({type}) => type);
+
+    expect(actions).toContain(setPromoFilm.toString());
+  });
+
+  it('should dispatch Logout when Delete /logout', async () => {
+    mockAPI
+      .onDelete(APIRoute.Logout)
+      .reply(204);
+
+    const store = mockStore();
+    Storage.prototype.removeItem = jest.fn();
+
+    await store.dispatch(logoutAction());
+
+    const actions = store.getActions().map(({type}) => type);
+
+    expect(actions).toContain(requireAuthorization.toString());
+
+    expect(Storage.prototype.removeItem).toBeCalledTimes(1);
+    expect(Storage.prototype.removeItem).toBeCalledWith('wtw-token');
   });
 });
